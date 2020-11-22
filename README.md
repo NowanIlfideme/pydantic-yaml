dc6cc5c50b0c3df691fd52e0d3d17c6ee1930c32
Anatoly Makarevich <anatoly_mak@yahoo.com>
2020-11-22 19:31:10 +0300

    Add readme. Add classifiers to setup.py

---
+++ README.md
@@ -0,0 +1,16 @@
+# pydantic-yaml
+
+This is a small helper library to.
+
+`pip install pydantic_yaml`
+
+Make sure to install `ruamel.yaml` or `pyyaml` as well. These are optional dependencies:
+
+`pip install pydantic_yaml[ruamel]`
+
+or, for development:
+
+```sh
+git clone https
+pip install -e .
+```
--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ from setuptools import setup
 
 from pydantic_yaml import __version__
 
-description = "Blah"
+path_readme = Path(__file__).parent / "README.md"
+
+description = "Adds some YAML functionality to the excellent `pydantic` library."
+
+with path_readme.open() as f:
+    long_description = f.read()
 
 setup(
     name="pydantic_yaml",
     version=str(__version__),
     description=description,
-    long_description=description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Anatoly Makarevich",
     author_email="anatoly_mak@yahoo.com",
@@ -26,8 +31,25 @@ setup(
     install_requires=["pydantic"],
     extras_require={
         "pyyaml": ["pyyaml"],
-        "ruamel-old": ["ruamel.yaml<0.15"],  # In-place 
+        "ruamel-old": ["ruamel.yaml<0.15"],  # In-place repalcement
         "ruamel-new": ["ruamel.yaml>=0.15"],  # Using new API starting 0.15
         "development": ["black", "flake8", "bump2version"],
     },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet",
+    ],
 )
