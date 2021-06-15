33e34d9ceaa252238b20c9b8dbe88c340c3a3909
Anatoly Makarevich <git@nowan.dev>
2021-06-15 21:38:03 +0200

    Added test for nested models.

--- pydantic_yaml/test/test_basic.py
+++ pydantic_yaml/test/test_basic.py
@@ -19,3 +19,34 @@ def test_readme():
 
     m3 = MyModel.parse_raw(yml, content_type="application/yaml")
     assert m1 == m3
+
+
+def test_nested_models():
+    """Test nested YAML models"""
+
+    from pydantic import BaseModel
+    from pydantic_yaml import YamlModel
+
+    class A(YamlModel):
+        x: int = 1
+
+    class B(BaseModel):
+        y: str = "hello"
+
+    class C(YamlModel):
+        a: A
+        b: B
+
+    c = C(a=A(x=11), b=B(y="bye"))
+
+    yml = c.yaml()
+
+    c_re = C.parse_raw(yml, proto="yaml")
+    assert c == c_re
+
+
+# def test_recursive_yaml():
+#     """TODO: Test how we parse recursive YAML documents."""
+
+# def test_files():
+#     """TODO: Test with tempfiles."""
