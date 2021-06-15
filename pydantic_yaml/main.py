79288e7d79241ab663ed263b7c961d5241079c44
Anatoly Makarevich <git@nowan.dev>
2021-06-16 00:41:01 +0200

    Added semver definition & more representers.

--- pydantic_yaml/main.py
+++ pydantic_yaml/main.py
@@ -2,7 +2,9 @@
 
 __all__ = ["__version__", "YamlEnum", "YamlModel"]
 
-from . import aux_types  # noqa
+from ._inject_representers import _inject_all
 from .enums import YamlEnum
 from .models import YamlModel
 from .version import __version__
+
+_inject_all()
