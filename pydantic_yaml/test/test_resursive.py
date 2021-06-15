94074b2bf4554c32a00aea12a9e3d54e8b562186
Anatoly Makarevich <git@nowan.dev>
2021-06-16 01:58:57 +0200

    Fix test: definitions.

--- pydantic_yaml/test/test_resursive.py
+++ pydantic_yaml/test/test_resursive.py
@@ -1,28 +1,27 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Optional
 
 import pytest
 
 from pydantic_yaml import YamlModel
 
 
+class A(YamlModel):
+    """Recursive model."""
+
+    a: int
+    inner: Optional["A"]
+
+
+A.update_forward_refs()
+
+
 def test_recursive_yaml():
     """Test how we react to (unsupported) recursive YAML documents."""
 
-    from typing import Optional
-
-    from pydantic_yaml import YamlModel
-
     file = Path(__file__).parent / "recursive.yaml"
-
-    class A(YamlModel):
-        """Recursive model."""
-
-        a: int
-        inner: Optional["A"]
-
-    A.update_forward_refs()
 
     with pytest.raises(ValueError):
         A.parse_file(file, content_type="application/yaml")
