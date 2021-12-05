03c5f0a7d503c5186cedba60f0bd0622080fe209
NowanIlfideme <git@nowan.dev>
2021-12-05 22:26:29 +0100

    Added YamlModel class.

---
+++ pydantic_yaml/model.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+from .mixin import YamlModelMixin
+
+_pre_doc = """`pydantic.BaseModel` class with built-in YAML support.
+
+You can alternatively inherit from `(pydantic.BaseModel, pydantic_yaml.YamlModelMixin)`
+to implement your model.
+
+See Also
+--------
+pydantic-yaml: https://github.com/NowanIlfideme/pydantic-yaml
+pydantic: https://pydantic-docs.helpmanual.io/
+pyyaml: https://pyyaml.org/
+ruamel.yaml: https://yaml.readthedocs.io/en/latest/index.html
+"""
+
+
+class YamlModel(BaseModel, YamlModelMixin):
+    __doc__ = _pre_doc + BaseModel.__doc__
