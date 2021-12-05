02a39c8f7f766b2572872a76c51305af5e6803ad
NowanIlfideme <git@nowan.dev>
2021-12-05 22:26:51 +0100

    Added classes.

--- pydantic_yaml/__init__.py
+++ pydantic_yaml/__init__.py
@@ -7,11 +7,16 @@ __all__ = [
     "YamlIntEnum",
     "YamlStr",
     "YamlStrEnum",
+    "YamlModel",
+    "YamlModelMixin",
+    "YamlModelMixinConfig",
 ]
 
-from .compat.yaml_lib import yaml
-from .compat.types import YamlInt, YamlIntEnum, YamlStr, YamlStrEnum
 from .compat.hacks import inject_all as _inject_yaml_hacks
+from .compat.types import YamlInt, YamlIntEnum, YamlStr, YamlStrEnum
+from .compat.yaml_lib import yaml
+from .mixin import YamlModelMixin, YamlModelMixinConfig
+from .model import YamlModel
 from .version import __version__
 
 _inject_yaml_hacks()
