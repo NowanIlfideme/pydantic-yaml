38d6b0607ec28f50236dbf6146ddce21e2ca7b2e
NowanIlfideme <git@nowan.dev>
2021-12-06 01:43:57 +0100

    Turns out the Enum class has quirks in newer Python versions...

--- pydantic_yaml/compat/types.py
+++ pydantic_yaml/compat/types.py
@@ -2,7 +2,7 @@
 
 from enum import Enum
 from inspect import isabstract
-from typing import Dict
+from typing import Dict, Tuple, Union
 
 from .representers import register_int_like, register_str_like
 from .yaml_lib import yaml
@@ -20,6 +20,9 @@ class YamlStr(str):
             register_str_like(cls, method=cls._to_yaml_str)
         return res
 
+    # def __new__(cls, v):
+    #     return super(cls).__new__(v)
+
     @classmethod
     def _to_yaml_str(cls, v) -> str:
         """Represent a value of this type as a string."""
@@ -33,6 +36,13 @@ class YamlStr(str):
         """Repr string that keeps the class type. You may redefine this."""
         return type(self).__qualname__ + "(" + super().__repr__() + ")"
 
+    def __getnewargs__(self) -> Tuple[str]:
+        """Implementation to prevent Enum from sabotaging us.
+        
+        I'm not joking! See `_make_class_unpicklable()` from `enum.py`
+        """
+        return super().__getnewargs__()
+
 
 class YamlInt(int):
     """An `int` subclass that serializes to YAML as an integer."""
@@ -44,6 +54,9 @@ class YamlInt(int):
             register_int_like(cls, method=cls._to_yaml_int)
         return res
 
+    # def __new__(cls, v):
+    #     return super(cls).__new__(v)
+
     @classmethod
     def _to_yaml_int(cls, v) -> int:
         """Represent a value of this type as an integer."""
@@ -53,12 +66,19 @@ class YamlInt(int):
         return super().__int__()
 
     def __str__(self) -> str:
-        """This returns the int-wrapped string."""
+        """This returns the wrapped int."""
         return super().__str__()
 
     def __repr__(self) -> str:
         """Repr string that keeps the class type. You may redefine this."""
         return type(self).__qualname__ + "(" + super().__repr__() + ")"
+
+    def __getnewargs__(self) -> Tuple[int]:
+        """Implementation to prevent Enum from sabotaging us.
+        
+        I'm not joking! See `_make_class_unpicklable()` from `enum.py`
+        """
+        return super().__getnewargs__()
 
 
 class YamlStrEnum(YamlStr, Enum):
@@ -73,6 +93,17 @@ class YamlStrEnum(YamlStr, Enum):
             vals: Dict[str, cls] = dict(cls.__members__)
             yaml.safe_dump(vals)
         return res
+
+    # def __new__(cls, v):
+    #     return super(cls).__new__(v)
+
+    def __str__(self) -> str:
+        """This returns the wrapped string."""
+        return str.__str__(self)
+
+    def __repr__(self) -> str:
+        """Repr string that keeps the class type. You may redefine this."""
+        return type(self).__qualname__ + "(" + str.__repr__(self) + ")"
 
 
 class YamlIntEnum(YamlInt, Enum):
