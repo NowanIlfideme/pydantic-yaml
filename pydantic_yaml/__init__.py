81bdb66326f2bf2c4ab423c09b7ec02ab0d2c017
Tom Wojcik <me@tomwojcik.com>
2022-12-28 21:23:21 +0100

    rm error silencing, raise fast if no yaml lib exists

--- pydantic_yaml/__init__.py
+++ pydantic_yaml/__init__.py
@@ -2,33 +2,33 @@
 
 from .version import __version__
 
-try:
-    __all__ = [
-        "__version__",
-        "SemVer",
-        "yaml",
-        "VersionedYamlModel",
-        "YamlEnum",  # deprecated class
-        "YamlInt",
-        "YamlIntEnum",
-        "YamlModel",
-        "YamlModelMixin",
-        "YamlModelMixinConfig",
-        "YamlStr",
-        "YamlStrEnum",
-    ]
-    from .main import (
-        SemVer,
-        VersionedYamlModel,
-        YamlEnum,
-        YamlInt,
-        YamlIntEnum,
-        YamlModel,
-        YamlModelMixin,
-        YamlModelMixinConfig,
-        YamlStr,
-        YamlStrEnum,
-        yaml,
-    )
-except Exception:
-    pass
+from pydantic_yaml.compat.yaml_lib import __yaml_lib__ as _
+
+
+__all__ = [
+    "__version__",
+    "SemVer",
+    "yaml",
+    "VersionedYamlModel",
+    "YamlEnum",  # deprecated class
+    "YamlInt",
+    "YamlIntEnum",
+    "YamlModel",
+    "YamlModelMixin",
+    "YamlModelMixinConfig",
+    "YamlStr",
+    "YamlStrEnum",
+]
+from .main import (
+    SemVer,
+    VersionedYamlModel,
+    YamlEnum,
+    YamlInt,
+    YamlIntEnum,
+    YamlModel,
+    YamlModelMixin,
+    YamlModelMixinConfig,
+    YamlStr,
+    YamlStrEnum,
+    yaml,
+)
