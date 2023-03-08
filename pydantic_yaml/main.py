4b02bc98afac169e2725a25552ca8df537722bb2
Mustafa Soylu <m.soylu@fz-juelich.de>
2023-03-08 16:25:22 +0100

    dont import classes if semver import fails

--- pydantic_yaml/main.py
+++ pydantic_yaml/main.py
@@ -21,10 +21,15 @@ from .compat.old_enums import YamlEnum
 from .compat.hacks import inject_all as _inject_yaml_hacks
 from .compat.types import YamlInt, YamlIntEnum, YamlStr, YamlStrEnum
 from .compat.yaml_lib import yaml, yaml_safe_dump, yaml_safe_load
-from .ext.semver import SemVer
-from .ext.versioned_model import VersionedYamlModel
 from .mixin import YamlModelMixin, YamlModelMixinConfig
 from .model import YamlModel
 from .version import __version__
+
+try:
+    from .ext.semver import SemVer
+    from .ext.versioned_model import VersionedYamlModel
+except:
+    __all__.remove("SemVer")
+    __all__.remove("VersionedYamlModel")
 
 _inject_yaml_hacks()
