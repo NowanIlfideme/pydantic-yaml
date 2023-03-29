b3cf1033fc594b5e5ee119bd55075e313038cd2e
NowanIlfideme <git@nowan.dev>
2023-03-30 00:58:35 +0200

    Try again with the mypy version issue.

--- src/pydantic_yaml/version.py
+++ src/pydantic_yaml/version.py
@@ -1,22 +1,19 @@
 """Gets the version, either installed or dynamically."""
 
-# type: ignore
-
-
 __all__ = ["__version__"]
 
 
 try:
-    from setuptools_scm import get_version
+    from setuptools_scm import get_version  # noqa
 
     __version__ = get_version(root="../..", relative_to=__file__)
 except Exception:
     try:
-        from importlib.metadata import PackageNotFoundError, version  # noqa  # type: ignore
+        import importlib.metadata as _im  # noqa
     except ImportError:
-        from importlib_metadata import PackageNotFoundError, version  # noqa  # type: ignore
+        import importlib_metadata as _im  # noqa
 
     try:
-        __version__ = version("pydantic_yaml")
-    except PackageNotFoundError:  # type: ignore
+        __version__ = _im.version("pydantic_yaml")
+    except _im.PackageNotFoundError:  # type: ignore
         __version__ = "0.0.0"
