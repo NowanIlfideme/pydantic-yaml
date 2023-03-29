b4288b372a56561abcdb6c404f0b0bdda4ae5c00
NowanIlfideme <git@nowan.dev>
2023-03-30 00:48:59 +0200

    Fix importlib_metadata issue.

--- src/pydantic_yaml/version.py
+++ src/pydantic_yaml/version.py
@@ -2,13 +2,17 @@
 
 __all__ = ["__version__"]
 
-from importlib_metadata import PackageNotFoundError, version  # noqa
 
 try:
     from setuptools_scm import get_version
 
     __version__ = get_version(root="../..", relative_to=__file__)
 except Exception:
+    try:
+        from importlib.metadata import PackageNotFoundError, version  # noqa
+    except ImportError:
+        from importlib_metadata import PackageNotFoundError, version  # noqa
+
     try:
         __version__ = version("pydantic_yaml")
     except PackageNotFoundError:
