4eebad0865c817b35d5ec4f5862ebe1a34918d19
NowanIlfideme <git@nowan.dev>
2023-04-21 16:48:00 +0200

    Re-added deprecated types (for now).

--- src/pydantic_yaml/__init__.py
+++ src/pydantic_yaml/__init__.py
@@ -1,7 +1,23 @@
 """YAML-enabled Pydantic models."""
 
-__all__ = ["__version__", "parse_yaml_file_as", "parse_yaml_raw_as", "to_yaml_file", "to_yaml_str"]
+__all__ = [
+    # New API
+    "__version__",
+    "parse_yaml_file_as",
+    "parse_yaml_raw_as",
+    "to_yaml_file",
+    "to_yaml_str",
+    # TODO: Re-add
+    # YamlModelMixin ?
+    # YamlModel ?
+    # Deprecated classes
+    "YamlInt",
+    "YamlIntEnum",
+    "YamlStr",
+    "YamlStrEnum",
+]
 
+from .deprecated.types import YamlInt, YamlIntEnum, YamlStr, YamlStrEnum
 from .dumper import to_yaml_file, to_yaml_str
 from .loader import parse_yaml_file_as, parse_yaml_raw_as
 from .version import __version__
