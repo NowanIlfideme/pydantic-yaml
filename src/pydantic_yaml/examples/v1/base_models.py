679d540973c50fe929f4e5be5cf12b78dbf0ea31
NowanIlfideme <git@nowan.dev>
2023-07-15 17:49:39 +0200

    Attempt to let mypy understand conditional import.

--- src/pydantic_yaml/examples/v1/base_models.py
+++ src/pydantic_yaml/examples/v1/base_models.py
@@ -1,109 +1,98 @@
 """Models used for testing, just normal `pydantic.BaseModel` objects."""
 
 
-from typing import List, Optional, Union
+import pydantic
 
-from pydantic import BaseModel, Field
-from pydantic.types import SecretBytes, SecretStr
+if pydantic.__version__ < "2":
+    from typing import List, Optional, Union
 
-from pydantic_yaml.examples.common import MyIntEnum, MyStrEnum
+    from pydantic import BaseModel, Field
+    from pydantic.types import SecretBytes, SecretStr
 
+    from pydantic_yaml.examples.common import MyIntEnum, MyStrEnum
 
-class Empty(BaseModel):
-    """Empty model."""
+    class Empty(BaseModel):
+        """Empty model."""
 
+    class A(BaseModel):
+        """Class A."""
 
-class A(BaseModel):
-    """Class A."""
+        a: str
 
-    a: str
+    class B(BaseModel):
+        """Class B."""
 
+        b: str
 
-class B(BaseModel):
-    """Class B."""
+    class Recursive(BaseModel):
+        """Recursive model, which is actually unsupported."""
 
-    b: str
+        inner: Optional["Recursive"]
+        a: int  # Doesn't work!
 
+    Recursive.update_forward_refs()
 
-class Recursive(BaseModel):
-    """Recursive model, which is actually unsupported."""
+    class SecretTstModel(BaseModel):
+        """Normal model with secret fields. This can't be roundtripped."""
 
-    inner: Optional["Recursive"]
-    a: int  # Doesn't work!
+        ss: SecretStr
+        sb: SecretBytes
 
+    def _encode_secret(obj: Union[SecretStr, SecretBytes, None]) -> Union[str, bytes, None]:
+        """Encode secret value."""
+        if obj is None:
+            return None
+        return obj.get_secret_value()
 
-Recursive.update_forward_refs()
+    class SecretTstModelDumpable(SecretTstModel):
+        """Round-trippable model. This will save secret fields as the raw values."""
 
+        class Config:
+            """Configuration."""
 
-class SecretTstModel(BaseModel):
-    """Normal model with secret fields. This can't be roundtripped."""
+            json_encoders = {SecretStr: _encode_secret, SecretBytes: _encode_secret}
 
-    ss: SecretStr
-    sb: SecretBytes
+    class HasEnums(BaseModel):
+        """Base model with enums."""
 
+        opts: MyStrEnum
+        vals: List[MyIntEnum]
 
-def _encode_secret(obj: Union[SecretStr, SecretBytes, None]) -> Union[str, bytes, None]:
-    """Encode secret value."""
-    if obj is None:
-        return None
-    return obj.get_secret_value()
+    # pydantic v1
 
+    class _Name(BaseModel):
+        """First/last names."""
 
-class SecretTstModelDumpable(SecretTstModel):
-    """Round-trippable model. This will save secret fields as the raw values."""
+        given: str
+        family: str
 
-    class Config:
-        """Configuration."""
+    class UsesRefs(BaseModel):
+        """Example for the reference data."""
 
-        json_encoders = {SecretStr: _encode_secret, SecretBytes: _encode_secret}
+        bill_to: _Name = Field(alias="bill-to")
+        ship_to: _Name = Field(alias="ship-to")
 
+        class Config:
+            """Pydantic configuration class."""
 
-class HasEnums(BaseModel):
-    """Base model with enums."""
+            allow_population_by_field_name = True
 
-    opts: MyStrEnum
-    vals: List[MyIntEnum]
+    class CustomRootListStr(BaseModel):
+        """Model with a custom root type.
 
+        See Also
+        --------
+        https://docs.pydantic.dev/usage/models/#custom-root-types
+        """
 
-# pydantic v1
+        __root__: List[str]
 
+    class CustomRootListObj(BaseModel):
+        """Model with a custom root type, list of objects.
 
-class _Name(BaseModel):
-    """First/last names."""
+        See Also
+        --------
+        https://docs.pydantic.dev/usage/models/#custom-root-types
+        """
 
-    given: str
-    family: str
-
-
-class UsesRefs(BaseModel):
-    """Example for the reference data."""
-
-    bill_to: _Name = Field(alias="bill-to")
-    ship_to: _Name = Field(alias="ship-to")
-
-    class Config:
-        """Pydantic configuration class."""
-
-        allow_population_by_field_name = True
-
-
-class CustomRootListStr(BaseModel):
-    """Model with a custom root type.
-
-    See Also
-    --------
-    https://docs.pydantic.dev/usage/models/#custom-root-types
-    """
-
-    __root__: List[str]
-
-
-class CustomRootListObj(BaseModel):
-    """Model with a custom root type, list of objects.
-
-    See Also
-    --------
-    https://docs.pydantic.dev/usage/models/#custom-root-types
-    """
-
-    __root__: List[Union[A, B]]
+        __root__: List[Union[A, B]]
--- src/pydantic_yaml/examples/v2/base_models.py
+++ src/pydantic_yaml/examples/v2/base_models.py
@@ -1,150 +1,147 @@
 """Models used for testing, just normal `pydantic.BaseModel` objects."""
 
-from typing import List, Optional, Union
+import pydantic
 
-from pydantic import BaseModel, ConfigDict, Field, field_serializer, model_serializer, model_validator
-from pydantic.types import SecretBytes, SecretStr
+if pydantic.__version__ >= "2":
+    from typing import List, Optional, Union
 
-from pydantic_yaml.examples.common import MyIntEnum, MyStrEnum
+    from pydantic import (
+        BaseModel,
+        ConfigDict,
+        Field,
+        field_serializer,
+        model_serializer,
+        model_validator,
+    )
+    from pydantic.types import SecretBytes, SecretStr
 
+    from pydantic_yaml.examples.common import MyIntEnum, MyStrEnum
 
-class Empty(BaseModel):
-    """Empty model."""
+    class Empty(BaseModel):
+        """Empty model."""
 
+    class A(BaseModel):
+        """Class A."""
 
-class A(BaseModel):
-    """Class A."""
+        a: str
 
-    a: str
+    class B(BaseModel):
+        """Class B."""
 
+        b: str
 
-class B(BaseModel):
-    """Class B."""
+    class Recursive(BaseModel):
+        """Recursive model, which is actually unsupported."""
 
-    b: str
+        inner: Optional["Recursive"]
+        a: int  # Doesn't work!
 
+    Recursive.model_rebuild()
 
-class Recursive(BaseModel):
-    """Recursive model, which is actually unsupported."""
+    class SecretTstModel(BaseModel):
+        """Normal model with secret fields. This can't be roundtripped."""
 
-    inner: Optional["Recursive"]
-    a: int  # Doesn't work!
+        ss: SecretStr
+        sb: SecretBytes
 
+    def _encode_secret(obj: Union[SecretStr, SecretBytes, None]) -> Union[str, bytes, None]:
+        """Encode secret value."""
+        if obj is None:
+            return None
+        return obj.get_secret_value()
 
-Recursive.model_rebuild()
+    class SecretTstModelDumpable(SecretTstModel):
+        """Round-trippable model. This will save secret fields as the raw values."""
 
+        @field_serializer("ss", "sb")
+        def _serialize_secrets(self, secret: Union[SecretStr, SecretBytes], _info):
+            """Serialize secret fields.
 
-class SecretTstModel(BaseModel):
-    """Normal model with secret fields. This can't be roundtripped."""
+            See Also
+            --------
+            https://docs.pydantic.dev/latest/usage/serialization/#custom-serializers
+            """
+            return _encode_secret(secret)
 
-    ss: SecretStr
-    sb: SecretBytes
+    class HasEnums(BaseModel):
+        """Base model with enums."""
 
+        opts: MyStrEnum
+        vals: List[MyIntEnum]
 
-def _encode_secret(obj: Union[SecretStr, SecretBytes, None]) -> Union[str, bytes, None]:
-    """Encode secret value."""
-    if obj is None:
-        return None
-    return obj.get_secret_value()
+    class _Name(BaseModel):  # type: ignore[no-redef]
+        """First/last names."""
 
+        given: str
+        family: str
 
-class SecretTstModelDumpable(SecretTstModel):
-    """Round-trippable model. This will save secret fields as the raw values."""
+    class UsesRefs(BaseModel):  # type: ignore[no-redef]
+        """Example for the reference data."""
 
-    @field_serializer("ss", "sb")
-    def _serialize_secrets(self, secret: Union[SecretStr, SecretBytes], _info):
-        """Serialize secret fields.
+        bill_to: _Name = Field(alias="bill-to")
+        ship_to: _Name = Field(alias="ship-to")
+
+        # Pydantic config
+        model_config = ConfigDict(populate_by_name=True)
+
+    class CustomRootListStr(BaseModel):  # type: ignore[no-redef]
+        """Model with a custom root type.
 
         See Also
         --------
-        https://docs.pydantic.dev/latest/usage/serialization/#custom-serializers
+        https://docs.pydantic.dev/blog/pydantic-v2-alpha/#changes-to-basemodel
+        https://github.com/pydantic/pydantic/blob/2b9459f20d094a46fa3093b43c34444240f03646/tests/test_parse.py#L95-L113
         """
-        return _encode_secret(secret)
 
+        root: List[str]
 
-class HasEnums(BaseModel):
-    """Base model with enums."""
+        @model_validator(mode="before")
+        @classmethod
+        def _populate_root(cls, values):
+            if isinstance(values, CustomRootListStr):
+                return values
+            return {"root": values}
 
-    opts: MyStrEnum
-    vals: List[MyIntEnum]
+        @model_serializer(mode="wrap")
+        def _serialize(self, handler, info):
+            data = handler(self)
+            if info.mode == "json":
+                return data["root"]
+            else:
+                return data
 
+        @classmethod
+        def model_modify_json_schema(cls, json_schema):
+            """JSON schema changer."""
+            return json_schema["properties"]["root"]
 
-class _Name(BaseModel):  # type: ignore[no-redef]
-    """First/last names."""
+    class CustomRootListObj(BaseModel):  # type: ignore[no-redef]
+        """Model with a custom root type, list of objects.
 
-    given: str
-    family: str
+        See Also
+        --------
+        https://docs.pydantic.dev/blog/pydantic-v2-alpha/#changes-to-basemodel
+        https://github.com/pydantic/pydantic/blob/2b9459f20d094a46fa3093b43c34444240f03646/tests/test_parse.py#L95-L113
+        """
 
+        root: List[Union[A, B]]
 
-class UsesRefs(BaseModel):  # type: ignore[no-redef]
-    """Example for the reference data."""
+        @model_validator(mode="before")
+        @classmethod
+        def _populate_root(cls, values):
+            if isinstance(values, CustomRootListObj):
+                return values
+            return {"root": values}
 
-    bill_to: _Name = Field(alias="bill-to")
-    ship_to: _Name = Field(alias="ship-to")
+        @model_serializer(mode="wrap")
+        def _serialize(self, handler, info):
+            data = handler(self)
+            if info.mode == "json":
+                return data["root"]
+            else:
+                return data
 
-    # Pydantic config
-    model_config = ConfigDict(populate_by_name=True)
-
-
-class CustomRootListStr(BaseModel):  # type: ignore[no-redef]
-    """Model with a custom root type.
-
-    See Also
-    --------
-    https://docs.pydantic.dev/blog/pydantic-v2-alpha/#changes-to-basemodel
-    https://github.com/pydantic/pydantic/blob/2b9459f20d094a46fa3093b43c34444240f03646/tests/test_parse.py#L95-L113
-    """
-
-    root: List[str]
-
-    @model_validator(mode="before")
-    @classmethod
-    def _populate_root(cls, values):
-        if isinstance(values, CustomRootListStr):
-            return values
-        return {"root": values}
-
-    @model_serializer(mode="wrap")
-    def _serialize(self, handler, info):
-        data = handler(self)
-        if info.mode == "json":
-            return data["root"]
-        else:
-            return data
-
-    @classmethod
-    def model_modify_json_schema(cls, json_schema):
-        """JSON schema changer."""
-        return json_schema["properties"]["root"]
-
-
-class CustomRootListObj(BaseModel):  # type: ignore[no-redef]
-    """Model with a custom root type, list of objects.
-
-    See Also
-    --------
-    https://docs.pydantic.dev/blog/pydantic-v2-alpha/#changes-to-basemodel
-    https://github.com/pydantic/pydantic/blob/2b9459f20d094a46fa3093b43c34444240f03646/tests/test_parse.py#L95-L113
-    """
-
-    root: List[Union[A, B]]
-
-    @model_validator(mode="before")
-    @classmethod
-    def _populate_root(cls, values):
-        if isinstance(values, CustomRootListObj):
-            return values
-        return {"root": values}
-
-    @model_serializer(mode="wrap")
-    def _serialize(self, handler, info):
-        data = handler(self)
-        if info.mode == "json":
-            return data["root"]
-        else:
-            return data
-
-    @classmethod
-    def model_modify_json_schema(cls, json_schema):
-        """JSON schema changer."""
-        return json_schema["properties"]["root"]
+        @classmethod
+        def model_modify_json_schema(cls, json_schema):
+            """JSON schema changer."""
+            return json_schema["properties"]["root"]
