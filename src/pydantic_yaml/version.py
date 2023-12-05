67ea0bb6157c00bd1e33f96048cf5bb00ce2fcc3
Branch Vincent <branchevincent@gmail.com>
2023-12-04 21:34:41 -0800

    deps: drop importlib_metadata
    This is now included in all supported python versions, starting with 3.8: https://docs.python.org/3/library/importlib.metadata.html

--- src/pydantic_yaml/version.py
+++ src/pydantic_yaml/version.py
@@ -2,24 +2,18 @@
 
 __all__ = ["__version__"]
 
-from typing import no_type_check
 
-
-@no_type_check
 def __get_version() -> str:
     try:
         from setuptools_scm import get_version  # noqa
 
         vv = get_version(root="../..", relative_to=__file__)
     except Exception:
-        try:
-            import importlib.metadata as _im  # noqa
-        except ImportError:
-            import importlib_metadata as _im  # noqa
+        import importlib.metadata as _im
 
         try:
             vv = _im.version("pydantic_yaml")
-        except _im.PackageNotFoundError:  # noqa
+        except _im.PackageNotFoundError:
             vv = "0.0.0"
     return vv
 
