167ccf961c190768de213ee0ed9661266b9d8a31
NowanIlfideme <git@nowan.dev>
2024-03-29 19:41:49 +0100

    Change to use internal implementation.

--- src/pydantic_yaml/__init__.py
+++ src/pydantic_yaml/__init__.py
@@ -17,7 +17,25 @@ __all__ = [
     "YamlStrEnum",
 ]
 
+
+from pydantic.version import VERSION as PYDANTIC_VERSION
+
+if (PYDANTIC_VERSION > "1") and (PYDANTIC_VERSION < "2"):
+    from pydantic_yaml._internals.v1 import (
+        to_yaml_file,
+        to_yaml_str,
+        parse_yaml_file_as,
+        parse_yaml_raw_as,
+    )
+elif (PYDANTIC_VERSION > "2") and (PYDANTIC_VERSION < "3"):
+    from pydantic_yaml._internals.v2 import (
+        to_yaml_file,
+        to_yaml_str,
+        parse_yaml_file_as,
+        parse_yaml_raw_as,
+    )
+else:
+    raise ImportError(f"Unsupported version of Pydantic: {PYDANTIC_VERSION!r}")
+
 from .deprecated.types import YamlInt, YamlIntEnum, YamlStr, YamlStrEnum
-from .dumper import to_yaml_file, to_yaml_str
-from .loader import parse_yaml_file_as, parse_yaml_raw_as
 from .version import __version__
