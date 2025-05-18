8782c171142989d1450a93f0303e77d08d7f03b4
NowanIlfideme <git@nowan.dev>
2025-05-18 15:27:54 +0200

    Clean up what 'internals' mean.

--- src/pydantic_yaml/__init__.py
+++ src/pydantic_yaml/__init__.py
@@ -1,16 +1,17 @@
 """YAML-enabled Pydantic models."""
 
 __all__ = [
-    # New API
     "__version__",
+    # v1 functional API
     "parse_yaml_file_as",
     "parse_yaml_raw_as",
     "to_yaml_file",
     "to_yaml_str",
+    # v2 API - TBD
 ]
 
 
-from pydantic_yaml._internals.v2 import (
+from pydantic_yaml._internals.pydantic_v2 import (
     to_yaml_file,
     to_yaml_str,
     parse_yaml_file_as,
