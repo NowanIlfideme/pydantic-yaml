9043b5af2829696e570fbb8632b88daf8ad4576a
NowanIlfideme <git@nowan.dev>
2025-05-25 17:27:41 +0200

    Tried adding descriptions and docstrings as comments. Very icky.

--- src/pydantic_yaml/_internals/v2.py
+++ src/pydantic_yaml/_internals/v2.py
@@ -11,20 +11,26 @@ Roundtrip comments with ruamel.yaml
 # mypy: ignore-errors
 
 import json
+import warnings
+from collections.abc import Mapping, Sequence
 from io import BytesIO, IOBase, StringIO
 from pathlib import Path
 from typing import Any, TypeVar
 from typing_extensions import Literal  # noqa
 
 from pydantic.version import VERSION as PYDANTIC_VERSION
-from ruamel.yaml import YAML
+from ruamel.yaml import CommentedMap, CommentedSeq, YAML
 
 if (PYDANTIC_VERSION < "2") or (PYDANTIC_VERSION > "3"):
     raise ImportError("This module can only be imported in Pydantic v2.")
 
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel, RootModel, TypeAdapter
 from pydantic.v1 import BaseModel as BaseModelV1
 from pydantic.v1 import parse_obj_as
+from pydantic.v1.fields import FieldInfo as FieldInfoV1
+from pydantic.fields import FieldInfo
+
+from .comments import CommentsOptions
 
 T = TypeVar("T", bound=BaseModel | BaseModelV1)
 
@@ -41,10 +47,84 @@ def _chk_model(model: Any) -> tuple[BaseModel | BaseModelV1, Literal[1, 2]]:
     )
 
 
+def _get_doc(
+    obj: BaseModel | BaseModelV1 | FieldInfo | FieldInfoV1 | Any, opts: CommentsOptions
+) -> str | None:
+    """Get documentation for the model or field, taking options into account."""
+    if isinstance(obj, BaseModel | BaseModelV1):
+        if opts in (True, "models-only"):
+            return getattr(obj, "__doc__", None)
+        return None
+    elif isinstance(obj, FieldInfo | FieldInfoV1):
+        if opts in (True, "fields-only"):
+            return obj.description
+        return None
+    return None
+
+
+def _add_descriptions(
+    ystruct: CommentedMap | CommentedSeq,  # or other stuff...
+    obj: BaseModel | BaseModelV1 | Mapping | Sequence | Any,
+    opts: CommentsOptions,
+) -> None:
+    """Add descriptions from the object to the yaml struct (modifying it).
+
+    This will fail if `ystruct` and `obj` don't have the same structure.
+    """
+    # Add top-level comment
+    top_lvl = _get_doc(obj, opts=opts)
+    if top_lvl is not None:
+        try:
+            indent = ystruct.lc.col  # type: ignore  # HACK
+        except Exception:
+            indent = 0
+        ystruct.yaml_set_start_comment(top_lvl, indent=indent)
+
+    if isinstance(obj, BaseModel):
+        if obj.__pydantic_root_model__:
+            # RootModel should probably work
+            assert isinstance(obj, RootModel), "Model incorrectly set as RootModel."
+            obj = obj.root
+    elif isinstance(obj, BaseModelV1):
+        if obj.__custom_root_type__:
+            # RootModel should probably work
+            obj = obj.__dict__["__root__"]
+
+    if isinstance(obj, BaseModel | BaseModelV1):
+        # Add field information
+        flds: dict[str, FieldInfo] | dict[str, FieldInfoV1]
+
+        if isinstance(obj, BaseModel):
+            flds = obj.model_fields
+        elif isinstance(obj, BaseModelV1):
+            flds = obj.__fields__
+
+        for fld_name, fld_info in flds.items():
+            if fld_name in ystruct.keys():
+                # Add field description (if any/allowed)
+                fld_desc = _get_doc(fld_info, opts=opts)
+                if fld_desc is not None:
+                    ystruct.yaml_add_eol_comment(fld_desc, key=fld_name)
+
+                # Recurse into fields
+                fld_obj = getattr(obj, fld_name, None)
+                if isinstance(fld_obj, BaseModel | BaseModelV1 | Sequence | Mapping):
+                    _add_descriptions(ystruct[fld_name], fld_obj, opts=opts)
+                # otherwise - no additional descriptions
+    elif isinstance(obj, Sequence) and isinstance(ystruct, CommentedSeq):
+        # Recurse into parts of the sequence; needed in cases of `list[dict[str, MyModel]]` and such
+        for i, sub_obj in enumerate(obj):
+            _add_descriptions(ystruct[i], sub_obj, opts=opts)
+    elif isinstance(obj, Mapping) and isinstance(ystruct, CommentedMap):
+        for key, sub_obj in obj.items():
+            _add_descriptions(ystruct[key], sub_obj, opts=opts)
+
+
 def _write_yaml_model(
     stream: IOBase,
     model: BaseModel | BaseModelV1,
     *,
+    add_comments: CommentsOptions = False,
     default_flow_style: bool | None = None,
     indent: int | None = None,
     map_indent: int | None = None,
@@ -63,6 +143,8 @@ def _write_yaml_model(
         The stream to write to.
     model : BaseModel
         The model to write.
+    add_comments : False or True or "fields-only" or "models-only"
+        Whether to add comments to the output YAML using fields and/or model descriptions.
     default_flow_style : bool
         Whether to use "flow style" (more human-readable).
         https://yaml.readthedocs.io/en/latest/detail.html?highlight=default_flow_style#indentation-of-block-sequences
@@ -74,7 +156,7 @@ def _write_yaml_model(
         An instance of ruamel.yaml.YAML (or a subclass) to use as the writer.
         The above options will be set on it, if given.
     json_kwargs : Any
-        Keyword arguments to pass `model.json()`.
+        Keyword arguments to pass `model.model_dump_json()`.
     """
     model, vers = _chk_model(model)
     if vers == 1:
@@ -88,19 +170,41 @@ def _write_yaml_model(
     elif isinstance(custom_yaml_writer, YAML):
         writer = custom_yaml_writer
     else:
-        raise TypeError(f"Please pass a YAML instance or subclass. Got {custom_yaml_writer!r}")
+        raise TypeError(
+            f"Please pass a YAML instance or subclass. Got {custom_yaml_writer!r}"
+        )
     # Set options
     if default_flow_style is not None:
         writer.default_flow_style = default_flow_style
     writer.indent(mapping=indent, sequence=indent, offset=indent)
-    writer.indent(mapping=map_indent, sequence=sequence_indent, offset=sequence_dash_offset)
+    writer.indent(
+        mapping=map_indent, sequence=sequence_indent, offset=sequence_dash_offset
+    )
     # TODO: Configure writer further?
-    writer.dump(val, stream)
+    if add_comments is False:
+        writer.dump(val, stream)
+    elif add_comments in (True, "fields-only", "models-only"):
+        # We need to roundtrip!
+        temp_stream = StringIO()
+        writer.dump(val, temp_stream)
+        rt_yaml = YAML(typ="rt", pure=True)
+        ystruct = rt_yaml.load(temp_stream.getvalue())
+        if isinstance(ystruct, CommentedMap | CommentedSeq):
+            _add_descriptions(ystruct, obj=model, opts=add_comments)
+            rt_yaml.dump(ystruct, stream)
+        else:
+            # Don't know what to do with this; just write the original value
+            warnings.warn(
+                "Failed to add comments to model; is not a map or sequence.",
+                category=UserWarning,
+            )
+            writer.dump(val, stream)
 
 
 def to_yaml_str(
     model: BaseModel | BaseModelV1,
     *,
+    add_comments: CommentsOptions = False,
     default_flow_style: bool | None = False,
     indent: int | None = None,
     map_indent: int | None = None,
@@ -115,6 +219,8 @@ def to_yaml_str(
     ----------
     model : BaseModel
         The model to convert.
+    add_comments : False or "all" or "fields-only" or "models-only"
+        Whether to add comments to the output YAML using fields and/or model descriptions.
     default_flow_style : bool
         Whether to use "flow style" (more human-readable).
         https://yaml.readthedocs.io/en/latest/detail.html?highlight=default_flow_style#indentation-of-block-sequences
@@ -126,7 +232,7 @@ def to_yaml_str(
         An instance of ruamel.yaml.YAML (or a subclass) to use as the writer.
         The above options will be set on it, if given.
     json_kwargs : Any
-        Keyword arguments to pass `model.json()`.
+        Keyword arguments to pass `model.model_dump_json()`.
 
     Notes
     -----
@@ -138,6 +244,7 @@ def to_yaml_str(
     _write_yaml_model(
         stream,
         model,
+        add_comments=add_comments,
         default_flow_style=default_flow_style,
         indent=indent,
         map_indent=map_indent,
@@ -154,6 +261,7 @@ def to_yaml_file(
     file: Path | str | IOBase,
     model: BaseModel | BaseModelV1,
     *,
+    add_comments: CommentsOptions = False,
     default_flow_style: bool | None = False,
     indent: int | None = None,
     map_indent: int | None = None,
@@ -170,6 +278,8 @@ def to_yaml_file(
         The file path or stream to write to.
     model : BaseModel
         The model to write.
+    add_comments : False or "all" or "fields-only" or "models-only"
+        Whether to add comments to the output YAML using fields and/or model descriptions.
     default_flow_style : bool
         Whether to use "flow style" (more human-readable).
         https://yaml.readthedocs.io/en/latest/detail.html?highlight=default_flow_style#indentation-of-block-sequences
@@ -181,7 +291,7 @@ def to_yaml_file(
         An instance of ruamel.yaml.YAML (or a subclass) to use as the writer.
         The above options will be set on it, if given.
     json_kwargs : Any
-        Keyword arguments to pass `model.json()`.
+        Keyword arguments to pass `model.model_dump_json()`.
 
     Notes
     -----
@@ -190,6 +300,7 @@ def to_yaml_file(
     """
     model, _ = _chk_model(model)
     write_kwargs = dict(
+        add_comments=add_comments,
         default_flow_style=default_flow_style,
         indent=indent,
         map_indent=map_indent,
