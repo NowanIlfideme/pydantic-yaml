e4dca86bc3defcc6bd38ee27bc97b624d5cb04ce
Anatoly Makarevich <git@nowan.dev>
2026-04-08 23:25:44 +0200

    Switch to uv for dependency and environment management (#323)
    * Add uv. Switch to hatchling build backend and hatch-vcs (tried dunamai but didn't work out for some reason).  Pinned Pydantic <3 and Mkdocs<2.
    
    * Remove old junk that was mistakenly committed.
    
    * Set Python version to 3.10 by default. Ensure Hatchling is there for development (to get dynamic version).
    
    * Update readthedocs config to use uv
    
    * Remove mamba/conda environments
    
    * Update testing and publishing to use uv (probably doesn't work and I'll need another PR)

--- src/pydantic_yaml/version.py
+++ src/pydantic_yaml/version.py
@@ -1,21 +1,68 @@
-"""Gets the version, either installed or dynamically."""
+"""Gets the version, either installed or dynamically.
+
+Adapted from:
+https://github.com/maresb/hatch-vcs-footgun-example/blob/main/hatch_vcs_footgun_example/version.py
+"""
 
 __all__ = ["__version__"]
 
 
+def __get_hatch_version() -> str:
+    """Compute the most up-to-date version number in a development environment.
+
+    For more details, see <https://github.com/maresb/hatch-vcs-footgun-example/>.
+    """
+    import os
+    from pathlib import Path
+
+    from hatchling.metadata.core import ProjectMetadata
+    from hatchling.plugin.manager import PluginManager
+    from hatchling.utils.fs import locate_file
+
+    pyproject_toml = locate_file(__file__, "pyproject.toml")
+    if pyproject_toml is None:
+        raise RuntimeError("pyproject.toml not found although hatchling is installed")
+    root = Path(pyproject_toml).parent
+
+    # Temporarily set cwd to project root for PEP 517 compliance.
+    old_cwd = Path.cwd()
+    os.chdir(root)
+    try:
+        metadata = ProjectMetadata(root=str(root), plugin_manager=PluginManager())
+        # Version can be static in pyproject.toml or computed dynamically:
+        return metadata.core.version or metadata.hatch.version.cached
+    finally:
+        os.chdir(old_cwd)
+
+
+def __get_importlib_metadata_version():
+    """Compute the version number using importlib.metadata.
+
+    This is the official Pythonic way to get the version number of an installed
+    package. However, it is only updated when a package is installed. Thus, if a
+    package is installed in editable mode, and a different version is checked out,
+    then the version number will not be updated.
+    """
+    from importlib.metadata import version
+
+    if __package__ is None:
+        raise RuntimeError(
+            f"__package__ not set in '{__file__}' - ensure that you are running this "
+            "module as part of a package, e.g. 'python -m mypackage.version' instead "
+            "of 'python mypackage/version.py'."
+        )
+    __version__ = version(__package__)
+    return __version__
+
+
 def __get_version() -> str:
     try:
-        from setuptools_scm import get_version  # noqa
-
-        vv = get_version(root="../..", relative_to=__file__)
+        return __get_hatch_version()
     except Exception:
-        import importlib.metadata as _im
-
         try:
-            vv = _im.version("pydantic_yaml")
-        except _im.PackageNotFoundError:
-            vv = "0.0.0"
-    return vv
+            return __get_importlib_metadata_version()
+        except Exception:
+            return "0.0.0"
 
 
 __version__ = __get_version()
